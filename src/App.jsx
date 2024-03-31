4fb70c1091d06fe03ee0b341299b194f13dd9723
kurtCoding <kjulien7790@gmail.com>
2024-03-31 12:09:57 -0400

    corrected connections

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./index.css";
-import Contacts from "./Contacts.jsx";
-import NavBar from "./NavBar.jsx";
-import Posts from "./Posts.jsx"
-import UserProfile from "./UserProfile.jsx";
+import Contacts from "./Components/Contacts.jsx";
+import NavBar from "./Components/NavBar.jsx";
+import Posts from "./Components/Posts.jsx"
+import UserProfile from "./Components/UserProfile.jsx";
 
 function App() {
   return (
--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,1 +1,10 @@
+const Post = () => {
+    return (
+        <li>
+            <h1>Title</h1>
+            <h2>Text</h2>
+        </li>
+    )
+}
+
 export default Post;
